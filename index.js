4041b7fff4a31a11882291df434f6e29fabaf78b
hajinaka44-boop <hajinaka44@gmail.com>
2025-11-13 02:00:03 +0700

    Update emoji for Isriwil country code

--- index.js
+++ index.js
@@ -262,7 +262,7 @@ const COUNTRY_CODES = {
 
   55:"Brazil 🇧🇷",1:"USA 🇺🇸",44:"UK 🇬🇧",33:"France 🇫🇷",49:"Germany 🇩🇪",977:"Nepal 🇳🇵",
 
-  7:"Russia 🇷🇺",92:"Pakistan 🇵🇰",234:"Nigeria 🇳🇬",254:"Kenya 🇰🇪",213:"Algeria 🇩🇿",249:"Sudan 🇸🇩",20:"Egypt 🇪🇬",972:"Isriwil 💩"
+  7:"Russia 🇷🇺",92:"Pakistan 🇵🇰",234:"Nigeria 🇳🇬",254:"Kenya 🇰🇪",213:"Algeria 🇩🇿",249:"Sudan 🇸🇩",20:"Egypt 🇪🇬",972:"Isriwil 🇮🇱💩"
 
 };
 
